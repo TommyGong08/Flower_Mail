24b60c50afea35de528514a30a998267a66b3be1
GongHailong <1034231958@qq.com>
2020-09-15 16:02:29 +0800

    add Delete draft

--- client/Module/source/WriteModule.c
+++ client/Module/source/WriteModule.c
@@ -64,9 +64,17 @@ char*  LeadinDraft(UserInfo* userInfo)
 }
 
 //从本地文件夹删除草稿
-int DeleteDraft()
+int DeleteDraft(UserInfo* userInfo)
 {
-   
+   char path[50];
+   sprintf(path, "../data/%s/draft/1.txt",userInfo->UserName);
+   if(remove(path) == 0){
+      printf("remove draft success\n");
+      return 0;
+   }else {
+      printf("remove draft failed\n");
+      return -1;
+   }
 }
 
 //从本地得到附件,读取到text中,(success)
@@ -156,20 +164,12 @@ int SendEmail(MailInfo*  EmailInfo, char* text)
 	int length=0;
 	char* msg=NULL;
 	int client_socket=0;
-   if(EmailInfo->IfAttachFile == 0)//有附件
-   {
-       sprintf(buffer,"emailclitoser|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|^",
+   sprintf(buffer,"emailclitoser|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|",
                                  EmailInfo->EmailID,EmailInfo->EmailTheme,EmailInfo->EmailPath,EmailInfo->EmailType,
                                  EmailInfo->EmailState,EmailInfo->CopySendID,EmailInfo->SecretSendID,EmailInfo->EmailSystemTime,
-                                 EmailInfo->IfAttachFile,EmailInfo->EmailID,EmailInfo->AttachFilePath,EmailInfo->EmailSender,EmailInfo->EmailReceiver,text);
-   }else if(EmailInfo->IfAttachFile == -1){
-      sprintf(buffer,"emailclitoser|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|",
-                                 EmailInfo->EmailID,EmailInfo->EmailTheme,EmailInfo->EmailPath,EmailInfo->EmailType,
-                                 EmailInfo->EmailState,EmailInfo->CopySendID,EmailInfo->SecretSendID,EmailInfo->EmailSystemTime,
-                                 EmailInfo->IfAttachFile,EmailInfo->EmailID,EmailInfo->AttachFilePath,EmailInfo->EmailSender,EmailInfo->EmailReceiver,text);
-   }
-	client_socket= connect_socket(SERVER_IP,SERVER_PORT);   
-
+                                 EmailInfo->IfAttachFile,EmailInfo->EmailID,EmailInfo->AttachFilePath,EmailInfo->EmailSender,
+                                 EmailInfo->EmailReceiver,text);
+	client_socket= connect_socket(SERVER_IP,SERVER_PORT);  
 	printf ("string sended to server:%s\n",buffer);
 	length=send_msg(client_socket,buffer,FILE_BUFFER_SIZE);
    if(length<0){
@@ -178,11 +178,13 @@ int SendEmail(MailInfo*  EmailInfo, char* text)
    }
 	bzero (buffer, FILE_BUFFER_SIZE);
    printf("SERVERIP=%s   SERVERPORT=%d",SERVER_IP,SERVER_PORT);
-	length = recv_msg(client_socket,buffer,FILE_BUFFER_SIZE);//从服务器端接受到emailId
+//	length = recv_msg(client_socket,buffer,FILE_BUFFER_SIZE);//从服务器端接受到emailId
+   length = recv_msg(client_socket, buffer, LONG_CONTENT_SIZE);
    if(length < 0){
       printf("can't receive message from server!\n");
       return -1;
    }
+   close_socket(client_socket);
    int send_attachfile = 0;
    //判断邮件类型，如果有附件
    if(EmailInfo->IfAttachFile==0){
@@ -194,6 +196,5 @@ int SendEmail(MailInfo*  EmailInfo, char* text)
           printf("send attached file success!\n");
        }
    }
- close_socket(client_socket);
 return 0;
 }
